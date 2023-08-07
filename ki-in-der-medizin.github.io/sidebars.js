966437f83f7ffa613f641792490a817fd833a554
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-08-07 21:44:34 +0200

    refactor: add zusammenfassung and bemerkung to sidebar

--- ki-in-der-medizin.github.io/sidebars.js
+++ ki-in-der-medizin.github.io/sidebars.js
@@ -14,6 +14,7 @@ const sidebars = {
                 "kapitel/neuron_als_mathematisches_modell/index",
              //   "kapitel/neuronale_netze_und_deep_learning/index",
              //   "kapitel/anwendung_in_der_medizin/index",
+                "kapitel/zusammenfassung_und_bemerkungen/index",
             ]
         },
         "literaturverzeichnis/index"
